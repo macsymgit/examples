af22d8e4920cbb79b160b620b31a4864ca3ea8c1
macsymwang <56002160+macsymwang@users.noreply.github.com>
2021-11-13 07:37:35 -0500

    init

---
+++ beam/src/test/java/com/mwang/examples/dataflow/beam/BeamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mwang.examples.dataflow.beam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
